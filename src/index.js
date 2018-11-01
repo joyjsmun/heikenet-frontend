d700d7226ed637b67ea1f9b921bf4ea7ad6f5f72
Joy <joy.js.mun@gmail.com>
2018-11-02 02:43:58 +0800

    ADD : reactstrap library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
+import './styles/main.scss';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
